3e478b6c812b05a5ef2d02b76c50539809859722
Thiago Canato de Azevedo <thiago.azevedo@gruposese.com>
2026-02-11 13:52:49 -0300

    refactor(assembly): improved assembly route logs and route description

--- api/routes/assembly.py
+++ api/routes/assembly.py
@@ -11,59 +11,59 @@ router = APIRouter()
 log = logger("assembly")
 
 
-@router.get("/response/json", summary="Get Response From Assembly Line API")
+@router.get("/response/json", summary="Get response from Assembly Line API")
 def get_json_response(api: AccessAssemblyLineApi = Depends(DependeciesInjection.get_api)):
-    log.info("Rota /response/json chamada — iniciando coleta de JSON da Assembly Line API")
+    log.info("GET assembly/response/json — started collecting JSON from Assembly Line API")
 
     try:
         response = api.get_json_response()
-        log.info("JSON obtido com sucesso da Assembly Line API")
+        log.info("Sucessfully returned JSON from Assembly Line API")
         return response
 
     except Exception as e:
-        log.error("Erro ao obter JSON da Assembly Line API", exc_info=True)
-        raise HTTP_Exceptions().http_502("Erro ao buscar origem: ", e)
+        log.error("Error trying to get JSON from Assembly Line API", exc_info=True)
+        raise HTTP_Exceptions().http_502("Error trying to get JSON from Assembly Line API: ", e)
 
 
-@router.get("/response/processed", summary="Get Processed Response From Assembly Line API")
+@router.get("/response/processed", summary="Get processed response from Assembly Line API JSON")
 def get_processed_response(
     api: AccessAssemblyLineApi = Depends(DependeciesInjection.get_api),
     limit: int = Query(50, ge=1, le=100000)
 ):
-    log.info(f"Rota /response/processed chamada — limit={limit}")
+    log.info(f"GET assembly/response/processed — limit={limit}")
 
     try:
         df = BuildPipeline().build_assembly(api)
-        log.info(f"Processamento concluído — total de registros: {df.height}")
+        log.info(f"Processing finished — amount of registers: {df.height}")
         return df.head(limit).to_dicts()
 
     except Exception as e:
-        log.error("Erro ao processar registros da Assembly Line API", exc_info=True)
-        raise HTTP_Exceptions().http_500("Erro ao processar registros:", e)
+        log.error("Error processing Assembly Line JSON", exc_info=True)
+        raise HTTP_Exceptions().http_500("Error processing Assembly Line JSON:", e)
 
 
-@router.post("/upsert", summary="Upsert Assembly Line Values In The DataBase")
+@router.post("/upsert", summary="Upsert JSON values from Assembly Line in database")
 def upsert_assembly(
     api: AccessAssemblyLineApi = Depends(DependeciesInjection.get_api),
     upsert: UpsertInfos = Depends(DependeciesInjection.get_upsert),
     batch_size: int = Query(10000, ge=1, le=100000)
 ):
-    log.info(f"Rota POST /upsert chamada — batch_size={batch_size}")
+    log.info(f"POST assembly/upsert — batch_size={batch_size}")
 
     try:
         df = BuildPipeline().build_assembly(api)
-        log.info(f"Pipeline executado com sucesso — registros processados: {df.select(pl.len()).collect().item()}")
+        log.info(f"Assembly pipeline successfully executed — amount of registers: {df.select(pl.len()).collect().item()}")
 
         rows = upsert.upsert_df("assembly_line", df, batch_size)
-        log.info(f"Upsert realizado com sucesso — linhas gravadas: {rows}")
+        log.info(f"Successfully upserted values — amount of registers: {rows}")
 
         return {
-            "message": "Upsert concluído com sucesso.",
+            "message": "Successfully upserted values",
             "rows": rows,
             "batch_size": batch_size,
             "table": "assembly_line",
         }
 
     except Exception as e:
-        log.error("Erro durante operação de upsert no banco", exc_info=True)
-        raise HTTP_Exceptions().http_500("Erro no upsert:", e)
+        log.error("Error during upsert operation", exc_info=True)
+        raise HTTP_Exceptions().http_500("Error during upsert operation:", e)
